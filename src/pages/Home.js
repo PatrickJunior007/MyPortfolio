1bf4f6e2c8ce704adda4ea988b1a3acc068db900
unknown <ogambapatrickjunior@gmail.com>
2023-05-18 14:23:05 +0100

    Added TypeScript and new Cv'

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import Typed from "typed.js";
 import Image from "../assets/PJr.webp";
-import CV from "../assets/CV3.pdf";
+import CV from "../assets/CV4.pdf";
 import Me from "../assets/me.jpg";
 import "../css/popularity.css";
 import CardProject from "../components/card/CardProject";
@@ -228,7 +228,7 @@ const Home = () => {
                 include:
                 <strong style={{ color: "#0E5092" }}>
                   {" "}
-                  MongoDB, FireBase, Testing, Cucumber, Cypress
+                  MongoDB, TypeScript, FireBase, Testing, Cucumber, Cypress
                 </strong>
                 .
               </p>
